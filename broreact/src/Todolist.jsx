02a81bdcc8df572f3253d4fd3bc7987eeaf7c2ff
priya <priya211@gmail.com>
2025-07-31 01:07:05 +0530

    added todolist component

--- broreact/src/Todolist.jsx
+++ broreact/src/Todolist.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React ,{useState}from 'react'
 
 const Todolist = () => {
+    const [tasks,setTasks]=useState(["go to college","go to gym","go to market"]);
+    const [newTask,setNewTask]=useState("");
+    function handleinputChange(event){
+        setNewTask(event.target.value); // Update the newTask state with the input value
+    }
+    function handleAddTask(){
+        
+    }
+    function handledeletetask(index){
+
+    }
+    function moveTaskUp(index){
+
+    }
+    function moveTaskDown(index){
+
+    }
+
   return (
-    <div>Todolist</div>
+    <div className="to-do-list">
+        <h1>To-Do List</h1>
+        <input 
+            type="text" 
+            value={newTask} 
+            onChange={handleinputChange} 
+            placeholder="Add a new task" 
+        />
+        <button className="add-button"onClick={handleAddTask}>Add Task</button>
+        <ul>
+            {tasks.map((task, index) => (
+                <li key={index}>
+                    <span className="text">{task}</span>
+                    <button className="delete-button" onClick={() => handledeletetask(index)}>Delete</button>
+                    <button className="move-button" onClick={() => moveTaskUp(index)}>Up</button>
+                    <button  className="move-button"onClick={() => moveTaskDown(index)}>Down</button>
+                </li>
+            ))}
+        </ul>
+
+    </div>
+    
   )
 }
 
